10dc1b422e2d0ee1d9533d0d9740e3ff0b3f1adf
CHEN Qing <chenqing1213@gmail.com>
2013-11-17 14:09:29 +0800

    change symboic link with absolutely directory.

--- setup_env.sh
+++ setup_env.sh
@@ -6,7 +6,11 @@ gem source add http://ruby.taobao.org/
 
 sudo gem install chef --verbose --no-rdoc
 
-cd ../ && ln -s chef-repo /etc/chef && cd /etc/chef
+sudo rm -rf /tmp/chef-repo /etc/chef
+
+cp -R ../chef-repo /tmp/
+
+sudo ln -s /tmp/chef-repo /etc/chef && cd /etc/chef
 
 sudo chef-solo solo.rb
 

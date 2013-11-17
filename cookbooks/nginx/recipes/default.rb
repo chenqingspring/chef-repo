d5bfab6583217c5d9a08a80964bdbf57ade80b62
CHEN Qing <chenqing1213@gmail.com>
2013-11-17 13:39:45 +0800

    remove nginx start for mac.

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,6 +1,6 @@
 package 'nginx'
 
-service 'nginx' do
-  supports [:status, :restart, :reload]
-  action :start
-end
+#service 'nginx' do
+#  supports [:status, :restart, :reload]
+#  action :start
+#end
